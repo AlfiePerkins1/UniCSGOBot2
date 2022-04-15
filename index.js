8a8411fc6b0a124dd98be21ad13c16289de60038
AlfiePerkins1 <alfiejperkins@gmail.com>
2022-04-15 17:23:16 +0100

    Update index.js
    Added node-sheets and added code to get all rows from nse stat checking sheet

--- index.js
+++ index.js
@@ -4,61 +4,80 @@ const discordjs = require("discord.js");
 const dotenv = require("dotenv");
 //import * as fs from 'fs';
 const fs = require("fs");
-const axios = require("axios")
-const puppeteer = require('puppeteer')
+const axios = require("axios");
+const puppeteer = require('puppeteer');
 const client = new discordjs.Client({ intents: [ 'GUILDS', 'GUILD_MESSAGES' ] });
-
+const Sheets = require("node-sheets").default;
+const nseStatsRows = [];
 //client.commands = new discordjs.Collection();
 client.commands = new discordjs.Collection();
 dotenv.config();
 const prefix = '+';
 
 fs.readdir("./commands", (err, files) => {
-  if(err) console.log(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
-    console.log("Commands could not be loaded.");
-    return;
-  }
+	if(err) console.log(err);
+	let jsfile = files.filter(f => f.split(".").pop() === "js");
+	if(jsfile.length <= 0){
+		console.log("Commands could not be loaded.");
+		return;
+	}
 
-  jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!` );
-    client.commands.set(props.help.name, props);
-  });
+	jsfile.forEach((f, i) =>{
+		let props = require(`./commands/${f}`);
+		console.log(`${f} loaded!` );
+		client.commands.set(props.help.name, props);
+	});
 });
 
 
 client.on('ready', () => {
 
-    // When bot comes online
-    console.log(`Bot online as ${client.user.tag}`);
-    })
+	// When bot comes online
+	console.log(`Bot online as ${client.user.tag}`);
+
+	(async () => {  try {
+		const gs = new Sheets('1-OA3YpObVKpwi-Tx6F-F2dipr-oCIxPgiN-MxQXxpJM');
+		const authData = require('./GoogleCredentials.json');
+		await gs.authorizeJWT(authData);
+		const table = await gs.tables('Main - updated');
+		//console.log(table.headers);
+		//console.log(table.formats);
+		//console.log(table.rows);
+		//Adding all rows to an array
+		for (let i = 0; i < table.rows.length; i++) {
+			nseStatsRows[i] = table.rows[i];
+		}
+		console.log(nseStatsRows[14]);
+    console.log(nseStatsRows.length);
+	} catch (err) { 
+		console.error(err);  }})();
+
+});
 
 
 
 client.on('messageCreate', message => {
 
-  let messageArray = message.content.split(" ");
+	let messageArray = message.content.split(" ");
  
-  let cmd = messageArray[0];
+	let cmd = messageArray[0];
 
-  let args = messageArray.slice(1);
+	let args = messageArray.slice(1);
 
-  let commandfile = client.commands.get(cmd.slice(prefix.length));
-  //a little bit of data parsing/general checks
-  if(message.content.startsWith(prefix) && !message.author.bot) {   // Starts with prefix or author isnt bot
-    console.log(client.commands);
-    let commandfile = client.commands.get(cmd.slice(prefix.length));
-    if (commandfile) commandfile.run(client, message,args)
-  }
-  else {
+	let commandfile = client.commands.get(cmd.slice(prefix.length));
+	//a little bit of data parsing/general checks
+	if(message.content.startsWith(prefix) && !message.author.bot) {   // Starts with prefix or author isnt bot
+		console.log(client.commands);
+		let commandfile = client.commands.get(cmd.slice(prefix.length));
+		if (commandfile) commandfile.run(client, message,args);
+	}
+	else {
 
-    console.log( "Message sent by: " + message.author.username );
-  }
+		console.log( "Message sent by: " + message.author.username );
+	}
 
   
-})
+});
 
 
 client.login(process.env.TOKEN);
