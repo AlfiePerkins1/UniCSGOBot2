231f2afda3adc4911a008fb09e9dcae4fac1883e
AlfiePerkins1 <alfiejperkins@gmail.com>
2022-04-15 04:38:38 +0100

    Update nuelstats.js
    Fixed more bugs, added async timeout to prevent "undefined" issues and stats not being found.

--- commands/nuelstats.js
+++ commands/nuelstats.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer')
-const { MessageEmbed, MessageActionRow, MessageButton, Permissions, GUILD_MEMBERS  } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageButton, Permissions } = require('discord.js');
 const discordjs = require("discord.js");
 const { head } = require('request');
 
@@ -7,11 +7,23 @@ module.exports.run = async (Client, message, args) => {
     
     console.log(args)
 
+    function waitFor(delay) {
+      return new Promise(resolve => setTimeout(resolve, delay));
+  }
+
     async function start() {
+
+      message.reply('Loading...')
+  .then(msg => {
+    setTimeout(() => msg.delete(), 2000)
+  })
+
         const browser = await puppeteer.launch()
         const page = await browser.newPage()
         await page.goto(`https://stadistics.com/player/${args}`)
-      
+        
+          //Waiting 2seconds to make sure everything is loaded
+       await waitFor(2000)
       const data = await page.evaluate(() => {
         return Array.from(document.querySelectorAll("div.PlayerStats div.PSTop div.PSContent")).map(x => x.textContent)
       })
