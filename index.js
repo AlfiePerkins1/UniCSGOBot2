a55f9b6c60d404c3feccc6cb83d2ab5fe6b00633
AlfiePerkins1 <alfiejperkins@gmail.com>
2022-04-16 01:09:56 +0100

    Added server specific configs
    Can control commands
    prefix and more to come

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 const discordjs = require("discord.js");
 const dotenv = require("dotenv");
 const fs = require("fs");
 const axios = require("axios");
 const puppeteer = require('puppeteer');
 const mongoose = require("mongoose");
+const sConfig = require("./models/sConfig");
 const client = new discordjs.Client({ intents: [ 'GUILDS', 'GUILD_MESSAGES' ] });
 const Sheets = require("node-sheets").default;
 const nseStatsRows = [];
-const mongoose = require("mongoose");
 client.commands = new discordjs.Collection();
 dotenv.config();
+
+//Get from database soon
 const prefix = '+';
 
 fs.readdir("./commands", (err, files) => {
@@ -32,8 +35,9 @@ client.on('ready', () => {
 
 	// When bot comes online
 	console.log(`Bot online as ${client.user.tag}`);
+	mongoose.connect(process.env.dbToken, {useUnifiedTopology: true, useNewUrlParser: true });
 
-	
+
 
 });
 
@@ -41,23 +45,60 @@ client.on('ready', () => {
 
 client.on('messageCreate', message => {
 
-	let messageArray = message.content.split(" ");
+    	//Setting up server specific config file
+	sConfig.findOne({
+		serverN: message.guild.name, 
+		serverID: message.guild.id
+	}, (err, config) => {
+		if(err) console.log(err);
+		if(!config){
+			const newsConfig = new sConfig({
+				serverN: message.guild.name, 
+				serverID: message.guild.id,
+				prefix: "+",
+				commands: ["test.js","nuelstats.js"]
+        
+			});
+
+			newsConfig.save().catch(err => console.log(err));
+		}else{
+			//Do nothing currently
+		}
+
+		let messageArray = message.content.split(" ");
  
-	let cmd = messageArray[0];
+		let cmd = messageArray[0];
 
-	let args = messageArray.slice(1);
+		let args = messageArray.slice(1);
 
-	let commandfile = client.commands.get(cmd.slice(prefix.length));
-	//a little bit of data parsing/general checks
-	if(message.content.startsWith(prefix) && !message.author.bot) {   // Starts with prefix or author isnt bot
-		console.log(client.commands);
 		let commandfile = client.commands.get(cmd.slice(prefix.length));
-		if (commandfile) commandfile.run(client, message,args);
-	}
-	else {
+		//a little bit of data parsing/general checks
+		if(message.content.startsWith(prefix) && !message.author.bot) {   // Starts with prefix or author isnt bot
+			console.log(client.commands);
+			let commandfile = client.commands.get(cmd.slice(prefix.length));
+			sConfig.findOne({
+				serverID: message.guild.id
+			}, (err, result) => {
+				console.log(`Commands ${result.commands}`);
 
-		console.log( "Message sent by: " + message.author.username );
-	}
+				console.log(`Includes; ${result.commands.includes(commandfile)}`)
+				if (commandfile && result.commands.includes(commandfile)) {
+					commandfile.run(client, message,args);
+				}
+				else {
+					message.reply(`This server does not have access to ${cmd}`)
+					console.log( "Message sent by: " + message.author.username );
+				}
+
+			});
+			
+		}
+		
+
+
+
+
+	});
 
   
 });
